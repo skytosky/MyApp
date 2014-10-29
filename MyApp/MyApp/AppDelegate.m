7eb63bca535008a288c1c2f4741b0e744c061f00
Wang Tonglong <wangtonglong@letv.com>
2014-10-29 17:04:44 +0800

    Add UI
    Add button action

--- MyApp/MyApp/AppDelegate.m
+++ MyApp/MyApp/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    UIButton *bnt =  [UIButton buttonWithType:UIButtonTypeCustom];
     return YES;
 }
 
--- MyApp/MyApp/ViewController.m
+++ MyApp/MyApp/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
     [self.view addSubview:btn];
+    UIButton *bnt =  [UIButton buttonWithType:UIButtonTypeCustom];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
